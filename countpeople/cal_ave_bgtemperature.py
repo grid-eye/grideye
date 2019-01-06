a49c0f6c3ca8ba7f4b1922a24de1386839cb8a5f
grid-eye <1600294912@qq.com>
2019-01-05 20:20:56 -1000

    plot the bgground and current frame

--- countpeople/cal_ave_bgtemperature.py
+++ countpeople/cal_ave_bgtemperature.py
@@ -12,27 +12,27 @@ amg = adafruit_amg88xx.AMG88XX(i2c)
 frame_x = 8
 frame_y = 8
 # we need th_bgframes frames  to calculate the average temperature of the bground
-th_bgframes = 30
+th_bgframes = 250
 # the counter of the bgframes frames
 bgframe_cnt = 0
-all_bgframes= []
-pre_read_count = 2
+all_bgframes = []
+pre_read_count = 30
 # 8X8 grid
 
-points = [(math.floor(ix/8),(ix%8)) for ix  in range(0,64)]
-grid_x,grid_y = np.mgrid[0:7:32j,0:7:32j]
+points = [(math.floor(ix/8), (ix % 8)) for ix in range(0, 64)]
+grid_x, grid_y = np.mgrid[0:7:32j, 0:7:32j]
 for i in range(pre_read_count):
     for row in amg.pixels:
         pass
 
 
-#插值(cublic)
-def cubicInterpolate(points , pixels , grid_x,grid_y , ip_type='cubic'):
-    return griddata(points,pixels,(grid_x,grid_y),method=ip_type)
-    
-    
+# 插值(cublic)
+def cubicInterpolate(points, pixels, grid_x, grid_y, ip_type='cubic'):
+    return griddata(points, pixels, (grid_x, grid_y), method=ip_type)
+
+
 while True:
-    temp = [] 
+    temp = []
     for row in amg.pixels:
         # Pad to 1 decimal place
         temp.append(row)
@@ -41,34 +41,36 @@ while True:
     if bgframe_cnt >= th_bgframes:
         print('next step')
         print(len(all_bgframes))
-        total_frames =  np.zeros((8,8))
-        for aitem in  range(len(all_bgframes)):
-            total_frames  = total_frames + np.array(all_bgframes[aitem])
+        total_frames = np.zeros((8, 8))
+        for aitem in range(len(all_bgframes)):
+            total_frames = total_frames + np.array(all_bgframes[aitem])
         average_temperature = total_frames / th_bgframes
         print(average_temperature)
         print((np.array(points).shape, np.array(average_temperature).shape))
-        print(grid_x.shape,grid_y.shape)
+        print(grid_x.shape, grid_y.shape)
         # the result of the interpolating for the grid
         average_temp = np.array(average_temperature).flatten()
-        inter_result = cubicInterpolate(points,average_temp
-                ,grid_x,grid_y,'cubic')
+        inter_result = cubicInterpolate(
+            points, average_temp, grid_x, grid_y, 'cubic')
 
-        temp = cubicInterpolate(points,np.array(temp).flatten()
-                ,grid_x,grid_y,'cubic')
-        print("after interpolating , save the data in file:average_temperature.npy" )
-        np.save('/tmp/average_temperature.npy',inter_result)
+        temp = cubicInterpolate(points, np.array(
+            temp).flatten(), grid_x, grid_y, 'cubic')
+        print("after interpolating , save the data in file:avg_temp.npy")
+        np.save('avgtemp.npy', inter_result)
 
-        fig ,( ax1,ax2 ,ax3 )= plt.subplots(3,1)
-        ax1.imshow(inter_result ,cmap = 'hot',interpolation='bicubic')
+        fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+        ax1.imshow(inter_result, cmap='hot', interpolation='bicubic')
         ax1.set_xlabel('X')
         ax1.set_ylabel('Y')
-        ax1.axis([0,31,32,0])
-        ax2.hist(np.array(inter_result).ravel(),bins=256,range=(19,28),fc='k',ec='k')
-        
+        ax1.axis([0, 31, 32, 0])
+        ax2.hist(np.array(inter_result).ravel(),
+                 bins=256, range=(16, 20), fc='k', ec='k')
+
         ax2.set_xlabel('temperature')
         ax2.set_ylabel('%')
         temp_diff = np.array(temp) - inter_result
-        ax3.hist(temp_diff.ravel(),bins = 256,range = (-1.0,0.9),fc='k',ec='k')
+        ax3.hist(temp_diff.ravel(), bins=256,
+                 range=(-1.0, 0.9), fc='k', ec='k')
         ax3.set_xlabel('temperature-diff')
         ax3.set_ylabel('%')
         plt.show()
--- countpeople/countpeople.py
+++ countpeople/countpeople.py
@@ -15,54 +15,57 @@ frame_y = 8
 th_bgframes = 30
 # the counter of the bgframes frames
 bgframe_cnt = 0
-all_bgframes= []
+all_bgframes = []
 pre_read_count = 2
 # 8X8 grid
 
-points = [(math.floor(ix/8),(ix%8)) for ix  in range(0,64)]
+points = [(math.floor(ix/8), (ix % 8)) for ix in range(0, 64)]
 # me_x = 8
 frame_y = 8
 # we need th_bgframes frames  to calculate the average temperature of th$
 th_bgframes = 30
 # the counter of the bgframes frames
 bgframe_cnt = 0
-all_bgframes= []
+all_bgframes = []
 pre_read_count = 2
 # 8X8 grid
 
-points = [(math.floor(ix/8),(ix%8)) for ix  in range(0,64)]
-grid_x,grid_y = np.mgrid[0:7:32j,0:7:32j]
+points = [(math.floor(ix/8), (ix % 8)) for ix in range(0, 64)]
+grid_x, grid_y = np.mgrid[0:7:32j, 0:7:32j]
 for i in range(pre_read_count):
-        for row in amg.pixels:
-                    pass
+    for row in amg.pixels:
+        pass
 
-grid_x,grid_y = np.mgrid[0:7:32j,0:7:32j]
-#discard the first and the second frame
+grid_x, grid_y = np.mgrid[0:7:32j, 0:7:32j]
+# discard the first and the second frame
 for i in range(pre_read_count):
-        for row in amg.pixels:
-                    pass
+    for row in amg.pixels:
+        pass
+
 
 class CountPeople:
-    def __init__(self,pre_read_count=2,th_bgframes=20):
-        #the counter of the bgframes
+    def __init__(self, pre_read_count=30, th_bgframes=20):
+        # the counter of the bgframes
         self.i2c = busio.I2C(board.SCL, board.SDA)
         self.amg = adafruit_amg88xx.AMG88XX(i2c)
-        self.grid_x,self.grid_y=np.mgrid[0:7:32j,0:7:32j]
-        self.bgframe_cnt =0
-        self.all_bgframes=[] #save all frames which sensor read
-        self.pre_read_count=pre_read_count
+        self.grid_x, self.grid_y = np.mgrid[0:7:32j, 0:7:32j]
+        self.bgframe_cnt = 0
+        self.all_bgframes = []  # save all frames which sensor read
+        self.pre_read_count = pre_read_count
         self.th_bgframes = th_bgframes
-        #8*8 grid
-        self.points = [(math.floor(ix/8),(ix%8)) for ix  in range(0,64)]
-        #discard the first and the second frame
+        # 8*8 grid
+        self.points = [(math.floor(ix/8), (ix % 8)) for ix in range(0, 64)]
+        # discard the first and the second frame
         for i in range(self.pre_read_count):
             for row in self.amg.pixels:
                 pass
-    def interpolate(self, points ,pixels,grid_x,grid_y,inter_type='cubic'):
+
+    def interpolate(self, points, pixels, grid_x, grid_y, inter_type='cubic'):
         '''
         interpolating for the pixels,default method is cubic
         '''
-        return griddata(points,pixels,(grid_x,grid_y),method=inter_type)
+        return griddata(points, pixels, (grid_x, grid_y), method=inter_type)
+
     def readPixelsArray(self):
         '''
           func:  read pixels
@@ -70,6 +73,7 @@ class CountPeople:
           return :2-d array
         '''
         pass
+
     def calAverageTemp(self):
         '''
            func: calulate the temperature of n frames ,n >= 200
@@ -77,12 +81,13 @@ class CountPeople:
             return : 2-d array which is the average temperature of
                     n frames
         '''
-        if len(self.all_bgframes) <(self.th_bgframes):
+        if len(self.all_bgframes) < (self.th_bgframes):
             raise RuntimeError('the len of the all_bgframes is too small')
-        total_frames=np.zeros((8,8))
+        total_frames = np.zeros((8, 8))
         for aitem in range(len(self.all_bgframes)):
             total_frames = total_frames+np.array(self.all_bgframes[aitem])
         return total_frames/self.th_bgframes
+
     def detectionNoise(self):
         '''
             func:remove noise from image
@@ -90,31 +95,60 @@ class CountPeople:
             return :2-d array
         '''
         pass
+
     def process(self):
         '''
             main function
         '''
+
+        # load the avetemp.py stores the average temperature
+        # the result of the interpolating for the grid
+        average_temperature = np.load("avgtemp.npy")
         while True:
-            temp = []
+            currFrame = []
             for row in self.amg.pixels:
-            #Pad to 1 decimal place
-                temp.append(row)
-            self.bgframe_cnt=self.bgframe_cnt+1
-            self.all_bgframes.append(temp)
-            if self.bgframe_cnt >= self.th_bgframes:
-                average_temperature=self.calAverageTemp()
-                # the result of the interpolating for the grid
-                average_temp = np.array(average_temperature).flatten()
-                inter_result = self.interpolate(points,average_temp,grid_x,grid_y,'cubic')
-                np.save('/tmp/average_temperature.npy',inter_result)
-                print(average_temp)
-                print("after save")
-                break
-    def  extractBody(self):
+                # Pad to 1 decimal place
+                currFrame.append(row)
+            currFrame = np.array(currFrame)
+            print(currFrame)
+            currFrameIntepol = self.interpolate(
+                points, currFrame.flatten(), grid_x, grid_y, 'cubic')
+            print('after interpolate')
+            plt.subplot(1, 2, 1)
+            print('average_temperature')
+            print(average_temperature)
+            plt.imshow(average_temperature)
+            plt.title('background temperature')
+            plt.subplot(122)
+            print('subplot 122')
+            plt.imshow(currFrameIntepol)
+            plt.title('current temperature')
+            fig, (bgaxes, cur_frames_axes,
+                  bg_cur_diff_axes) = plt.subplots(3, 1, num=2)
+            print(bgaxes)
+            bgaxes.set_xlim(16, 32)
+            print('after xlim')
+            bgaxes.hist(average_temperature.ravel(), bins=256,
+                        range=(15, 32), label='temperature hist')
+            print('hist')
+            cur_frames_axes.hist(currFrame, bins=255, range=(
+                15, 32), label='current temperature')
+            diff = currFrameIntepol - average_temperature
+            print('cal diff')
+            bg_cur_diff_axes.hist(diff.ravel(), range=(
+                -6, 6), label='difference between background temperature and current temperature')
+            plt.show()
+            print('nothing show')
+
+    def extractBody(self):
         pass
+
     def findBodyLocation(self):
         pass
+
     def trackPeople(self):
         pass
+
+
 countp = CountPeople()
 countp.process()

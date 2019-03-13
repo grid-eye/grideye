082fb1a4af7e590277a013115b93013b8a1ea1ba
grid-eye <1600294912@qq.com>
2019-03-13 23:56:29 +0800

    add knn

---
+++ countpeople/knn.py
@@ -0,0 +1,20 @@
+import numpy as np
+import os
+import sys
+def calculateFeature(allframe , avgtemp):
+    pass
+def getDataSet(path):
+    pass
+def getTestSet(path):
+    pass
+def normDataSet(dataSet):#归一化数据集
+    pass
+def knnClassify(traningSet,labels,testSet):
+    pass
+
+if __name__ == "__main__":
+    path = sys.argv[2]
+    allframe = np.load(path+"/imagedata.npy")
+
+
+

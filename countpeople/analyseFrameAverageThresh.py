2ec1d9ebfa3970dab229a18a217eb1aed076c907
grid-eye <1600294912@qq.com>
2019-04-04 22:35:17 +0800

    modify analyseMultiDirFrameAverageThresh

--- countpeople/analyseFrameAverageThresh.py
+++ countpeople/analyseFrameAverageThresh.py
@@ -15,8 +15,6 @@ def analyseSequence(allframe,avgtemp,argarray,show_frame=False,thresh=None ,inte
         curr_frame.append(allframe[i])
     diff_frame = np.round(np.array(diff_frame),1)
     for i in range(len(argarray)):
-        print("the %sth diff frame "%(argarray[i]))
-        print(allframe[argarray[i]])
         seq = argarray[i]
         currframe = diff_frame[i]
         img_ave = np.average(currframe)
@@ -27,7 +25,6 @@ def analyseSequence(allframe,avgtemp,argarray,show_frame=False,thresh=None ,inte
             bins = bins[:-1]
             rest = currframe.size - hists.sum() 
             hists[-1]+=rest
-            print(hists)
             ret,thre = otsuThreshold(currframe , 64)
             for i in range(len(bins)):
                 histMap[bins[i]] = int(hists[i])
@@ -47,28 +44,14 @@ def analyseSequence(allframe,avgtemp,argarray,show_frame=False,thresh=None ,inte
             plt.yticks([])
             plt.title("otsu binarize")
             plt.tight_layout()
-    #print(ave_arr)
     max_v = max(ave_arr)
     min_v = min(ave_arr)
-    print("====================max value of the ave_arr is %.2f================"%(max_v))
-    print("index of max value is")
-    print(ave_arr.index(max_v))
-    print("====================minimum value of the ave_arr is %.2f================"%(min_v))
-    print("index of min value is")
-    print(ave_arr.index(min_v))
     ave_array = np.array(ave_arr)
     curr_thresh = 0.375
     if thresh:
         curr_thresh = thresh
     sub_ave_index = np.where(ave_array>=curr_thresh)
     sub_ave = ave_array[sub_ave_index]
-    print("====================current thresh is %.3f============"%(curr_thresh))
-    print("=========over thresh ===============")
-    #print(sub_ave)
-    print("============sum of the length is %d============"%(ave_array.size))
-    print("==========len of the sub_ave is %d============="%(sub_ave.size))
-
-
     if show_frame:
         plt.show()
     return  ave_array.size,sub_ave.size
--- countpeople/analyseMultiFrameAverageThresh.py
+++ countpeople/analyseMultiFrameAverageThresh.py
@@ -5,36 +5,73 @@ from analyseFrameAverageThresh import analyseSequence
 #=============================================================================
 #=用于确定平均温度的阈值，有人的帧和无人的帧的平均温度的比较,测试数据有足够大=
 #=============================================================================
-path = sys.argv[1]
-dir_arr = sys.argv[2:]
-all_frame  =[]
-all_ave = []
-path_arr =[]
-for seq in dir_arr:
-    actual_dir = path+seq
-    actual_path = actual_dir+"/imagedata.npy"
-    path_arr.append(actual_path)
-    actual_avg_path=actual_dir+"/avgtemp.npy"
-    frames=np.load(actual_path)
-    avgframe = np.load(actual_avg_path)
-    all_ave.append(avgframe)
-    all_frame.append(frames)
-ret_arr = []#保存返回结果
-for i in range(len(all_frame)):
-    curr_path = path_arr[i]
-    print("analyse "+curr_path)
-    frame_seq = all_frame[i]
-    avgtemp = all_ave[i]
-    argarray = [i for i in range(len(all_frame[i]))]
+def getDefaultFgPath():
+    return [
+             ("images/2019-2-2-first",[1,5]),
+             ("test/2019-3-12-second-",[1,5]),
+             ("test/2019-3-19-first-",[1,2]),
+             ("test/2019-3-26-",[1,4]),
+             ("test/2019-3-31-high-",[1,4]),
+             ("test/2019-3-31-",[1,2])
+            ]
+
+def showThreshMap(thresh_map):
+    for k,v in thresh_map:
+        frame_sum = v[0]
+        over_thresh_sum = v[1]
+        print("thresh is %.3f "%(k))
+        print("=================frame sum is %d =================="%(frame_sum))
+        print("=================over thresh_sum is %d=============="%(over_thresh_sum))
+        print("==================frame with man accuracy is %.2f====================="%(over_thresh_sum/frame_sum))
+        print("===================frame without human accuracy is %.2f================="%((frame_sum-over_thresh_sum)/frame_sum))
+def analyseMultiDirFrame(path,start,end,thresh_arr,bg):
+    avgtemp = np.load(path+str(start)+"/avgtemp.npy")
+    ret_arr = []#保存返回结果
+    all_frame = []
     show_frame = False
-    ret = analyseSequence(frame_seq,avgtemp,argarray,show_frame)
-    ret_arr.append(ret)
-frame_sum = 0
-over_thresh_sum= 0
-for fsum,over_thresh in ret_arr:
-    frame_sum += fsum
-    over_thresh_sum += over_thresh
-print("=================frame sum is %d =================="%(frame_sum))
-print("=================over thresh_sum is %d=============="%(over_thresh_sum))
-print("==================frame with man accuracy is %.2f====================="%(over_thresh_sum/frame_sum))
-print("===================frae without human accuracy is %.2f================="%((frame_sum-over_thresh_sum)/frame_sum))
+    for seq in range(start,end):
+        actual_dir = path+str(seq)
+        actual_path = actual_dir+"/imagedata.npy"
+        frame = np.load(actual_path)
+        if os.path.exists(actual_path) == False:
+            continue
+        if not  bg:
+            human_data = np.load(actual_dir+"/human_data.npy")
+            temp = []
+            for item in human_data:
+                temp.append(frame[item])
+            all_frame += temp
+        else:
+            all_frame += frame.tolist()
+    all_frame = np.array(all_frame)
+    print(all_frame.shape)
+    thresh_map =[]
+    seqarray = [i for i in range(all_frame.shape[0])]
+    for item in thresh_arr:
+        ret = analyseSequence(all_frame,avgtemp,seqarray , show_frame,thresh=item)
+        thresh_map.append((item,ret))
+    showThreshMap(thresh_map)
+    return thresh_map
+path = sys.argv[1]
+if sys.argv[1] == "human":
+    bg=False
+else:
+    s = 2
+    bg=True
+thresh_arr = [0.2, 0.25,0.3,0.35,0.4]
+if bg:
+    start ,end  = [int(s) for s in sys.argv[s:s+2]]
+    result_map = analyseMultiDirFrame(path,start,end,thresh_arr)
+else:
+    fgPaths = getDefaultFgPath()
+    final_thresh_map = {}
+    for path,ranges in fgPaths:
+        ret = analyseMultiDirFrame(path,ranges[0],ranges[1],thresh_arr,bg)
+        if len(final_thresh_map) == 0:
+            final_thresh_map=dict(ret)
+        else:
+            for k,num_arr in ret:
+                v = final_thresh_map[k]
+                final_thresh_map[k] = [v[0]+num_arr[0],v[1]+num_arr[1]]
+    result_map  = sorted(final_thresh_map.items(),key=lambda d:d[1])
+showThreshMap(result_map)
